d125fa7ef41b84316d3ef27d3177335f176f5e6d
amnon <amnontamsut66@gmail.com>
2025-05-22 14:25:41 +0300

    Movie App initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY="1a24886158ce74b8b28c292c4ed6ea34"
+const BASE_URL="https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+}
+export const searchMovies = async (query) => {
+    const response = await fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+      )}`
+    );
+    const data = await response.json();
+    return data.results;
+  };
+
